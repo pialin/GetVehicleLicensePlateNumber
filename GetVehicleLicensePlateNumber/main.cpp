4b596ad6872ec0e88a8372023a1f13f4316057cd
pialin <765422908@qq.com>
2017-06-16 04:31:47 +0800

    完成定位标题草稿

--- GetVehicleLicensePlateNumber/main.cpp
+++ GetVehicleLicensePlateNumber/main.cpp
@@ -9,15 +9,15 @@ using namespace std;
 using namespace cv;
 
 
-int main( int ArgumentCount, char** ArgumentVector )
+int main(int ArgumentCount, char** ArgumentVector)
 {
 
 
 	//检查命令行所带参数数目是否正确，如果不正确则显示用法说明并退出程序
-	if( ArgumentCount != 2)
+	if (ArgumentCount != 2)
 	{
 		//显示程序用法说明
-		cout << " Usage:  "<< ArgumentVector[0] << " ImageToLoadAndDisplay" << endl;
+		cout << " Usage:  " << ArgumentVector[0] << " ImageToLoadAndDisplay" << endl;
 		//返回错误码并退出程序
 		return -1;
 	}
@@ -29,15 +29,15 @@ int main( int ArgumentCount, char** ArgumentVector )
 	RawImageMat = imread(
 		ArgumentVector[1],//输入图片路径
 		CV_LOAD_IMAGE_UNCHANGED//以不修改通道类型的方式读取图片
-		);
+	);
 
 	//检查读入的Mat是否包含图像数据
-	if( !RawImageMat.data )
-	{ 
+	if (!RawImageMat.data)
+	{
 		//显示图片读取失败提示信息
-		cout << " Error:  Can't read image data from"<< ArgumentVector[1]  << endl;
+		cout << " Error:  Can't read image data from" << ArgumentVector[1] << endl;
 		//返回错误码并退出程序
-		return -1; 
+		return -1;
 	}
 
 
@@ -54,17 +54,17 @@ int main( int ArgumentCount, char** ArgumentVector )
 			RawImageMat,//输入图片矩阵
 			GrayImageMat,//输出图片矩阵 
 			COLOR_BGR2GRAY//将图片由BGR（OpenCV默认通道格式）转换成灰阶图像
-			);
+		);
 	}
 	//如果图像已经为4通道（包含alpha通道）图像，则将其转换成灰阶图像
 	else if (NumRawImageChannel == 4)
 	{
 		//将图片由BGRA转换成灰阶图像
-		cvtColor( 
+		cvtColor(
 			RawImageMat,
-			GrayImageMat, 
+			GrayImageMat,
 			COLOR_BGRA2GRAY//将图片由BGRA转换成灰阶图像
-			);
+		);
 	}
 	//如果图像已经为单通道灰阶图像，直接不作转换将RawImageMat赋给GrayImageMat
 	else if (NumRawImageChannel == 1)
@@ -73,7 +73,7 @@ int main( int ArgumentCount, char** ArgumentVector )
 	}
 
 	//如果通道数不为1,3或4，输出错误码并退出程序
-	else 
+	else
 	{
 		cout << "Unkown image channel type: " << RawImageMat.channels();
 	}
@@ -82,43 +82,44 @@ int main( int ArgumentCount, char** ArgumentVector )
 	//指定窗口名称
 	const char* WindowName = "get plate number from vehicle license";
 	//创建相应的窗口
-	namedWindow( 
+	namedWindow(
 		WindowName,//窗口名称
 		CV_WINDOW_NORMAL//此窗口根据图片大小显示图片，不可resize
-		);
+	);
+	resizeWindow(WindowName, GrayImageMat.cols * 600 / GrayImageMat.rows, 600);
 	//在上述窗口中显示灰阶图像
-	imshow( 
+	imshow(
 		WindowName,//希望绘制图片的窗口名称
 		GrayImageMat//希望绘制的图片矩阵
-		);
+	);
 
 	//等待键盘响应，参数0表示等待时间为无限长
 	waitKey(0);
 
 
-	//对图像进行3*3的高斯模糊
+	//对图像进行5*5的高斯模糊
 	Mat BlurGrayImageMat;
-	GaussianBlur( 
+	GaussianBlur(
 		GrayImageMat, //输入图片矩阵
 		BlurGrayImageMat, //输出图片矩阵
-		Size(3,3), //高斯核尺寸
+		Size(9, 9), //高斯核尺寸
 		0, //??高斯核X方向标准差 
 		0, //??高斯核Y方向标准差
 		BORDER_DEFAULT //??像素插值方式 
-		);
+	);
 
 	//在上述窗口中显示高斯模糊后的图像
-	imshow( 
+	imshow(
 		WindowName,//希望绘制图片的窗口名称
 		BlurGrayImageMat//希望绘制的图片矩阵
-		);
+	);
 
 	//等待键盘响应，参数0表示等待时间为无限长
 	waitKey(0);
 
 
 	//使用Sobel算子计算X（水平）和Y(垂直)方向梯度
-	Mat GrayImageGradX,GrayImageGradY;
+	Mat GrayImageGradX, GrayImageGradY;
 	//计算灰阶图像X方向梯度）
 	Sobel(
 		BlurGrayImageMat,
@@ -130,39 +131,37 @@ int main( int ArgumentCount, char** ArgumentVector )
 		1, //??scale为1
 		0, //??delta为0
 		BORDER_DEFAULT //??边界类型，像素插值方法
-		);//此语句相当于：Scharr( GrayImageMat, GrayImageGradX,  CV_16S , 0, 1, 1, 0, BORDER_DEFAULT);
+	);//此语句相当于：Scharr( GrayImageMat, GrayImageGradX,  CV_16S , 0, 1, 1, 0, BORDER_DEFAULT);
 
-	////convertScaleAbs( grad_x, abs_grad_x );
-
-	//同理计算灰阶图像Y方向的梯度
+//同理计算灰阶图像Y方向的梯度
 	Sobel(
 		BlurGrayImageMat,
-		GrayImageGradY, 
+		GrayImageGradY,
 		CV_16S,
 		0, //横向求导阶数为0（即不求导）
 		1, //纵向求导结束为1
-		CV_SCHARR, 
-		1, 
-		0, 
-		BORDER_DEFAULT 
-		);
+		CV_SCHARR,
+		1,
+		0,
+		BORDER_DEFAULT
+	);
 
 
 	//将X和Y方向的梯度值求绝对值后映射成8位无符号整数
-	Mat GrayImageGradX8U,GrayImageGradY8U;
-	convertScaleAbs( 
+	Mat GrayImageGradX8U, GrayImageGradY8U;
+	convertScaleAbs(
 		GrayImageGradX, //输入矩阵
 		GrayImageGradX8U //输出矩阵
-		);
+	);
 
-	convertScaleAbs( 
-		GrayImageGradY, 
-		GrayImageGradY8U 
-		);
+	convertScaleAbs(
+		GrayImageGradY,
+		GrayImageGradY8U
+	);
 
 	//将XY方向梯度的加权作为总梯度
 	Mat GrayImageEdge;
-	addWeighted( 
+	addWeighted(
 		GrayImageGradX8U, //输入矩阵1
 		0.5, //矩阵1权重
 		GrayImageGradY8U, //输入矩阵2
@@ -170,22 +169,22 @@ int main( int ArgumentCount, char** ArgumentVector )
 		0, //标量偏置值
 		GrayImageEdge, //输出矩阵
 		-1 //输出矩阵类型与矩阵1相同
-		);
+	);
 
 	//梯度矩阵二值化
 	threshold(
-		GrayImageEdge, //输入矩阵
+		GrayImageGradX8U, //输入矩阵
 		GrayImageEdge, //输出矩阵
 		128, //迭代初始阈值
 		255, //最大值（超过阈值将设为此值）
 		CV_THRESH_OTSU //阈值化选择的方法:Otsu法
-		);  
+	);
 
 	//显示边缘二值图像
-	imshow( 
+	imshow(
 		WindowName,
 		GrayImageEdge
-		);
+	);
 
 	//等待键盘响应，参数0表示等待时间为无限长
 	waitKey(0);
@@ -194,28 +193,82 @@ int main( int ArgumentCount, char** ArgumentVector )
 	//构造膨胀操作的结构元
 	Mat DilateElement = Mat(
 		1,//第一维（Y轴尺寸）
-		19,//第二维（X轴尺寸）
+		11,//第二维（X轴尺寸）
 		CV_8U,//矩阵类型：8位无符号整数
 		cvScalar(1)//填充的数值，全1
-		);  
-	Mat DilatedGrayImageEdge;  
+	);
+	Mat DilatedGrayImageEdge;
 	//进行膨胀操作  
 	dilate(
 		GrayImageEdge,//输入矩阵
 		DilatedGrayImageEdge,//输出矩阵
 		DilateElement,//膨胀结构元
-		Point(9,0), //膨胀元锚点
+		Point(4, 0), //膨胀元锚点
 		1 //进行膨胀的次数
-		);
+	);
 	//显示膨胀后的二值图像
-	imshow( 
+	imshow(
 		WindowName,
 		DilatedGrayImageEdge
-		);
+	);
 
 
 	//等待键盘响应，参数0表示等待时间为无限长
 	waitKey(0);
+
+
+	vector<vector<Point> > AllContour;
+	vector<Vec4i> ContourHierarchy;
+	//Extract the contours so that
+
+	vector<vector<Point> > contours;
+	findContours(DilatedGrayImageEdge, AllContour, ContourHierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+	for (size_t iContour = 0; iContour < AllContour.size(); iContour++)
+	{
+		//得到这个连通区域的外接矩形
+
+		RotatedRect  ContourRect = minAreaRect(AllContour[iContour]);
+
+		float ContourRectHeight = ContourRect.size.height;
+		float ContourRectWidth = ContourRect.size.width;
+		float ContourRectRotateAngle = ContourRect.angle;
+		float ContourRectArea = ContourRect.size.area;
+		float ContourRectCenterX = ContourRect.center.x;
+		float ContourRectCenterY = ContourRect.center.y;
+
+		bool FlagFoundTitle = false;
+
+		//如果高度不足，或者长宽比太小，认为是无效数据，否则把矩形画到原图上
+
+		if (
+			(fabs(ContourRectHeight / ContourRectWidth - aaa) / aaa <= 0.05 && fabs(ContourRectRotateAngle <= -85)) ||
+			(fabs(ContourRectWidth / ContourRectHeight - aaa) / aaa <= 0.05 && fabs(ContourRectRotateAngle >= -5)) &&
+			(fabs(ContourRectArea / (RawImageMat.rows*RawImageMat.cols) - bbb) / bbb <= 0.05*0.05) &&
+			(fabs(ContourRectCenterX - ccc) / ccc <= 0.05) &&
+			(fabs(ContourRectCenterY - ddd) / ddd <= 0.05)
+			)
+		{
+			FlagFoundTitle = true;
+			if (ContourRectRotateAngle <= -85)
+			{
+				ContourRectRotateAngle = ContourRectRotateAngle + 90;
+			}
+			else
+			{
+				ContourRectRotateAngle = fabs( ContourRectRotateAngle);
+			}
+		
+
+			EstimatedPlateCenterX = ContourRectCenterX - eee * RawImageMat.rows * sin(ContourRectRotateAngle/180*pi);
+			EstimatedPlateCenterY = ContourRectCenterY + eee * RawImageMat.cols * cos(ContourRectRotateAngle/180*pi);
+
+		}
+
+		for
+		approxPolyDP(Mat(AllContour[k]), contours[k], 3, true);
+
+	}
 
 	//返回0并正常退出程序
 	return 0;
